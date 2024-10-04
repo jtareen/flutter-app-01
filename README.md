7d60ff4d42efd08d36bdbed9ec8624e8af450e6f
Rizwanullah10 <rizwanullahkakar10@gmail.com>
2024-10-05 01:49:30 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lab3_task1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/lab3_task1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab3_task1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
